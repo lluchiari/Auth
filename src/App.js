24f2b0af3c1f537389f08f1b663f977cf459fbda
Lucas Luchiari <lluchiari@localhost.localdomain>
2017-11-21 17:54:33 -0200

    Moving on the text field

--- src/App.js
+++ src/App.js
@@ -1,13 +1,26 @@
 import React, { Component } from 'react';
-import { View, Text } from 'react-native';
+import { View } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+      apiKey: 'AIzaSyCfYpjgCiH2q8HmG5b6iwz6sRqAbY6UbaU',
+      authDomain: 'auth-606da.firebaseapp.com',
+      databaseURL: 'https://auth-606da.firebaseio.com',
+      projectId: 'auth-606da',
+      storageBucket: 'auth-606da.appspot.com',
+      messagingSenderId: '546070702290'
+    });
+  }
+
   render(){
     return(
       <View>
         <Header headerText="Authentication" />
-        <Text> Umm App aqui!</Text>
+        <LoginForm />
       </View>
     );
   }
