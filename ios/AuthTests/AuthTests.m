65b04d300ff4b7b5c1bd9b08f02eadabd130caf4
Lucas Luchiari <lluchiari@localhost.localdomain>
2017-11-21 16:39:26 -0200

    New Directory refactoring

--- ios/AuthTests/AuthTests.m
+++ ios/AuthTests/AuthTests.m
@@ -16,11 +16,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React Native!"
 
-@interface authTests : XCTestCase
+@interface AuthTests : XCTestCase
 
 @end
 
-@implementation authTests
+@implementation AuthTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
